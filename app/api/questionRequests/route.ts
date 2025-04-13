3a5ac3a57224294d0754047a8a127b1dae3f7b69
Rodrigo Rocha <rodrigorgs@gmail.com>
2025-04-13 08:31:37 -0300

    Send question request to LLM (but doesn't save yet)

--- app/api/questionRequests/route.ts
+++ app/api/questionRequests/route.ts
@@ -1,6 +1,8 @@
 import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
+import OpenAI from "openai";
+import { QuestionRequest } from "@/app/generated/prisma";
 
 export async function GET(req: Request) {
   try {
@@ -25,18 +27,61 @@ export async function POST(req: Request) {
   try {
     const user = await prisma.user.findUnique({ where: { email: session.user.email } });
     if (!user) {
-        return NextResponse.json({ error: "User not found" }, { status: 404 });
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
     }
 
-    const newTemplate = await prisma.questionRequest.create({
+    const newQuestionRequest = await prisma.questionRequest.create({
       data: {
         parameterValues,
         templateId,
         userId: user.id,
       },
     });
-    return NextResponse.json(newTemplate, { status: 201 });
+
+    const responseContent = await requestLLM(newQuestionRequest);
+    console.log(responseContent);
+
+    return NextResponse.json(newQuestionRequest, { status: 201 });
   } catch (error) {
     return NextResponse.json({ error: "Failed to create template" }, { status: 500 });
   }
+}
+
+async function generatePrompt(questionRequest: QuestionRequest) {
+  const template = await prisma.questionRequestTemplate.findUnique({
+    where: { id: questionRequest.templateId },
+  });
+  if (!template) {
+    throw new Error("Template not found");
+  }
+
+  const promptTemplate = template.promptTemplate;
+  const parameterValues = questionRequest.parameterValues;
+  const prompt = promptTemplate.replace(/\<(\w+)\>/g, (_, key) => {
+    const value = parameterValues.find((param) => param.name === key);
+    return value ? value.values[0] : "";
+  });
+  return prompt;
+}
+
+async function requestLLM(questionRequest: QuestionRequest) {
+  const prompt = await generatePrompt(questionRequest);
+  
+  console.log(prompt);
+
+  const openai = new OpenAI({
+    apiKey: process.env.DEEPSEEK_API_KEY,
+    baseURL: 'https://api.deepseek.com',
+  });
+
+  console.log('trying llm');
+  const completion = await openai.chat.completions.create({
+    messages: [{ role: 'system', content: prompt }],
+    model: 'deepseek-chat',
+    response_format: {
+      type: 'json_object'
+    }
+  });
+
+  return completion.choices[0].message.content;
 }
