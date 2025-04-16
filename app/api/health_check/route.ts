66b2e54071a545cc787c99d6795c94909909e1bf
Rodrigo Rocha <rodrigorgs@gmail.com>
2025-04-16 10:00:12 -0300

    Fix health check

--- app/api/health_check/route.ts
+++ app/api/health_check/route.ts
@@ -1,5 +1,8 @@
 import { NextResponse } from "next/server"
 
 export async function GET(req: Request) {
-    return NextResponse.json({ status: 'OK' }, { status: 200 })
+    return new Response("OK", {
+        status: 200,
+        headers: { "Content-Type": "text/plain" },
+    });
 }
