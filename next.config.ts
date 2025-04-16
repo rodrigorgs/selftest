25ce6e69d385c27acd8e8da80495089cbe2a6e7f
Rodrigo Rocha <rodrigorgs@gmail.com>
2025-04-16 01:59:45 -0300

    Disable eslint

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,13 @@
 import type { NextConfig } from "next";
 
+module.exports = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+}
+
 const nextConfig: NextConfig = {
   /* config options here */
 };
